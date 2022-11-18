905c4686109169819412c82ce6a661be34a38cee
jva411 <j.victoralves411@alu.ufc.br>
2022-11-18 14:46:00 -0300

    Using C compiled functions

--- utils/ray.py
+++ utils/ray.py
@@ -1,15 +1,27 @@
 import numba
 import numpy as np
 from utils import transforms
+from ctypes import c_void_p, c_double
 
 
-@numba.experimental.jitclass([
-    ('origin', numba.float64[::1]),
-    ('direction', numba.float64[::1]),
-    ('t', numba.float64)
-])
 class Ray:
     def __init__(self, origin: np.ndarray, direction: np.ndarray, t: float = np.inf):
         self.origin = origin
         self.direction = direction
-        self.t = t
+        self.__t = t
+        self.originP = c_void_p(self.origin.ctypes.data)
+        self.directionP = c_void_p(self.direction.ctypes.data)
+        self.tC = c_double(t)
+
+    @property
+    def t(self):
+        return self.__t
+
+    @t.setter
+    def t(self, t):
+        self.__t = t
+        self.tC = c_double(t)
+
+    @property
+    def hitting_point(self):
+        return self.origin + self.direction * self.t
